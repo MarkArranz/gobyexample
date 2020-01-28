3a2f5ec47acabc710a82f5c6ba232e97f4649fe8
Mark Arranz <marranz@firstam.com>
2020-01-27 16:05:37 -0800

    Add comments to files.

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -1,7 +1,22 @@
+// Our first program will print the classic "hello world" message.
+// Here's the full source code.
 package main
 
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
 }
+
+// To run the program, put the code in `hello-world.go` and use `go run`.
+
+// go run hello-world.go
+
+// Sometimes we'll want to build our programs into binaries.
+// We can do this using `go build`.
+
+// go build hello-world.go
+
+// We can then execute the built binary directly.
+
+// ./hello-world
